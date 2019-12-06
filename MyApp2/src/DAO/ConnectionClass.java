e9cef27717992ee1a40f57d62b06407fc30d9a73
US1E007 <55137881+US1E007@users.noreply.github.com>
2019-12-06 18:35:29 +0530

    firt proj

---
+++ MyApp2/src/DAO/ConnectionClass.java
@@ -0,0 +1,18 @@
+package DAO;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionClass {
+protected static Connection intializeConn() throws ClassNotFoundException, SQLException {
+	String name = "05XyRxBcfa";
+	String username = "05XyRxBcfa";
+	String password = "C7MMoHmL97";
+	String dbURL = "jdbc:mysql://remotemysql.com:3306/05XyRxBcfa?autoReconnect=true&useSSL=false"; 
+    Class.forName("com.mysql.jdbc.Driver");
+    Connection con = DriverManager.getConnection(dbURL, username, password);
+	return con;
+}
+}
+
